fa0b55d56cfe9f9ebb3ba4e29bc1b03c8dd6a81f
acc-lab <acceleration2020@gmail.com>
2021-09-17 12:00:30 +0800

    police movement fix

--- characters/police.js
+++ characters/police.js
@@ -33,16 +33,16 @@ function skeleton_police_walking_func(reload=0, //the runtime delay between two 
 					this.cst="police-idle";
 				}else{
 					this.cst="police-idle";
-					this.x+=3*dir;
+					this.x+=2*dir;
 				}
 			}
 			if(this.tick%(4*walking_cycle_time)==1+walking_cycle_time){
 				this.cst="police-walk1";
-				this.x+=3*dir;
+				this.x+=2*dir;
 			}
 			if(this.tick%(4*walking_cycle_time)==1+walking_cycle_time*2){
 				this.cst="police-walk2";
-				this.x+=3*dir;
+				this.x+=1*dir;
 			}
 			if(this.tick%(4*walking_cycle_time)==(1+walking_cycle_time*3)%(4*walking_cycle_time)){
 				this.cst="police-walk3";
