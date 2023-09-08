a2bfb2e35b87092086446e120588707e4deb615c
acc-lab <63785690+acc-lab@users.noreply.github.com>
2023-09-09 01:04:39 +0800

    prevent pinch-zooming

--- assets/cursor.js
+++ assets/cursor.js
@@ -5,6 +5,11 @@ var cursor_y;
 //cursor's click
 var new_cursor_click=false;
 
+//prevent pinch-zooming
+window.addEventListener('gesturestart', e => e.preventDefault());
+window.addEventListener('gesturechange', e => e.preventDefault());
+window.addEventListener('gestureend', e => e.preventDefault());
+
 //if the mouse is moved, update new cursor coordinate
 document.onmousemove=function(e) {
 	cursor_x = e.clientX - cv.offsetLeft + 450;
